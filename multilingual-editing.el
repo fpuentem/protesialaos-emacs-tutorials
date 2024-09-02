f4e75c4f1dbea3f3f2fd6ef5ea21aaf436c96b0d
Fab <fapuentem@uni.pe>
2024-09-02 15:41:03 -0300

    Multilingual editing video (12/126)

---
+++ multilingual-editing.el
@@ -0,0 +1,18 @@
+;; Happy hacking, renkel - Emacs ♥ you!
+
+;; It the default swiching input method
+(setq default-input-method "greek")
+;; Switch between English(\U) and the 'default-input-method'
+
+;; To define interactivelly the default-input-method
+;; C-u C-\
+
+;; M-x describe-input-method (help)_
+
+;; ipa
+;; /ɛɛ/
+;; ʌ
+;; ʃ
+
+;; Change input-method to greek ω only for one letter and then back to English.
+;; C-x \
